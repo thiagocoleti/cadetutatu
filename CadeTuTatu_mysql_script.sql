7c1c5eab4709cab7d13e7fdd4702a6b084d9b336
Thiago Adriano Coleti <tcoleti@gmail.com>
2020-04-27 09:31:50 -0300

    Add files via upload

---
+++ CadeTuTatu_mysql_script.sql
@@ -0,0 +1,74 @@
+CREATE TABLE `usuarios` (
+	`id_usuario` int NOT NULL AUTO_INCREMENT,
+	`nome_usuario` varchar(255) NOT NULL,
+	`email_usuario` varchar(255) NOT NULL,
+	`senha_usuario` varchar(255) NOT NULL,
+	`tipo_usuario` char(1) NOT NULL,
+	`depto_usuario` varchar(50) NOT NULL,
+	PRIMARY KEY (`id_usuario`)
+);
+
+CREATE TABLE `invertebrados` (
+	`id_invertebrado` int NOT NULL AUTO_INCREMENT,
+	`nome_vulgar` varchar(255) NOT NULL,
+	`nome_cientifico` varchar(255) NOT NULL,
+	`ordem` varchar(255) NOT NULL,
+	`familia` varchar(255) NOT NULL,
+	`habitat` varchar(255) NOT NULL,
+	`alimentacao` varchar(255) NOT NULL,
+	`habitos` varchar(255) NOT NULL,
+	`outras_informacoes` TEXT(500) NOT NULL,
+	`distribuicao_geografica` TEXT(500) NOT NULL,
+	PRIMARY KEY (`id_invertebrado`)
+);
+
+CREATE TABLE `invertebrados_imagens` (
+	`fot_inv_id` int NOT NULL AUTO_INCREMENT,
+	`id_invertebrado` int NOT NULL,
+	`fot_inv_caminho` varchar(255) NOT NULL,
+	PRIMARY KEY (`fot_inv_id`)
+);
+
+CREATE TABLE `invertebrados_referencias` (
+	`ref_inv_id` int NOT NULL AUTO_INCREMENT,
+	`ref_descricao` TEXT(500) NOT NULL,
+	`id_invertebrado` int NOT NULL,
+	PRIMARY KEY (`ref_inv_id`)
+);
+
+CREATE TABLE `vertebrados` (
+	`id_vertebrado` int NOT NULL AUTO_INCREMENT,
+	`nome_vulgar` varchar(255) NOT NULL,
+	`nome_cientifico` varchar(255) NOT NULL,
+	`ordem` varchar(255) NOT NULL,
+	`familia` varchar(255) NOT NULL,
+	`habitat` varchar(255) NOT NULL,
+	`alimentacao` varchar(255) NOT NULL,
+	`habitos` varchar(255) NOT NULL,
+	`outras_informacoes` TEXT(500) NOT NULL,
+	`distribuicao_geografica` TEXT(500) NOT NULL,
+	PRIMARY KEY (`id_vertebrado`)
+);
+
+CREATE TABLE `invertebrados_imagens copy` (
+	`id_invertebrado` int NOT NULL,
+	`fot_inv_caminho` varchar(255) NOT NULL,
+	`fot_ver_id` int NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`fot_ver_id`)
+);
+
+CREATE TABLE `invertebrados_referencias copy` (
+	`ref_inv_id` int NOT NULL AUTO_INCREMENT,
+	`ref_descricao` TEXT(500) NOT NULL,
+	`id_invertebrado` int NOT NULL,
+	PRIMARY KEY (`ref_inv_id`)
+);
+
+ALTER TABLE `invertebrados_imagens` ADD CONSTRAINT `invertebrados_imagens_fk0` FOREIGN KEY (`id_invertebrado`) REFERENCES `invertebrados`(`id_invertebrado`);
+
+ALTER TABLE `invertebrados_referencias` ADD CONSTRAINT `invertebrados_referencias_fk0` FOREIGN KEY (`id_invertebrado`) REFERENCES `invertebrados`(`id_invertebrado`);
+
+ALTER TABLE `invertebrados_imagens copy` ADD CONSTRAINT `invertebrados_imagens copy_fk0` FOREIGN KEY (`id_invertebrado`) REFERENCES `vertebrados`(`id_vertebrado`);
+
+ALTER TABLE `invertebrados_referencias copy` ADD CONSTRAINT `invertebrados_referencias copy_fk0` FOREIGN KEY (`id_invertebrado`) REFERENCES `vertebrados`(`id_vertebrado`);
+
--- invertebrado.php
+++ invertebrado.php
@@ -19,28 +19,65 @@
 //  echo $_acao;
 
     if ($_acao == "INCLUIR"){
-
+        //chama função INSERIR passando os parâmetros
     }
     else if ($_acao == "ALTERAR"){
-
+        //chama função ALTERAR passando os parâmetros
     }
     else if($_acao == "EXCLUIR"){
-
+        //chama função EXCLUIR passando os parâmetros
     }
     else if($_acao == "REFERENCIAS"){
-
+        //chama função para adicionar referências passando os parâmetros
     }
     else if($_acao == "IMAGENS"){
-
+        //chama função para adicionar imagens passando os parâmetros
+    }
+    else if ($_acao == "BUSCAR"){
+        //chama função para buscar informações do invertebrado passando os parâmetros
     }
 
 
 
 function inserir($nomevulgar, $nomecientifico, $ordem, $familia, $autor, $habitat, $alimentacao, $habitos, $distgeografica, $informacoes){
 
+       include 'conexaoMySQL.php';
+ 
+    //-- CAMINHO DA IMAGEM DA PLACA--
+ 
+    
+    
+    $sql = "insert into ....";
+            
+   
+    if (mysqli_query($conn, $sql)) {
+        
+            echo 'Aviso de confirmação de ação para o usuário';
+
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+
 }
 
 function alterar ($idinvertebrado, $nomevulgar, $nomecientifico, $ordem, $familia, $autor, $habitat, $alimentacao, $habitos, $distgeografica, $informacoes){
+       include 'conexaoMySQL.php';
+ 
+    //-- CAMINHO DA IMAGEM DA PLACA--
+ 
+    
+    
+    $sql = "update ....";
+            
+   
+    if (mysqli_query($conn, $sql)) {
+        
+            echo 'Aviso de confirmação de ação para o usuário';
+
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
 
 }
 
@@ -54,6 +91,24 @@ function inserirreferencia ($idinvertebrado, $referencias){
 }
 
 function inseririmagens($idinvertebrado, $imagens){
+
+}
+
+function buscarInvertebrado($idinvertebrado){
+        include 'conexaoMySQL.php';
+                  $sql = ".. ";
+
+                          try {
+                              $result = $conn->query($sql);
+                          } catch (mysqli_sql_exception $ex) {
+                              $ex->getMessage();
+                            echo $ex;
+                          }
+
+                           if ($result->num_rows > 0) {
+                                while ($row = $result->fetch_assoc()) {  
+                                    //executa carga de objeto para retorno!!
+                            }
 
 }
 
