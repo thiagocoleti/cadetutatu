7be2bf73c2ceab83b9bbbec41b8e8d84ef2eea8c
Thiago Adriano Coleti <thiago.coleti@uenp.edu.br>
2022-01-30 16:29:57 -0300

    Perfil de Usuário e Senha
    Visualização de perfil de usuário;
    Alteração de senhas.

---
+++ menu_usuario.php
@@ -0,0 +1,7 @@
+ <a class="dropdown-item" href="detalhe_usuario.php"><i class="fas fa-id-badge fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Ver Perfil
+                </a>  
+<a  class="dropdown-item" href="alterar_senha.php"><i class="fas fa-lock fa-sm fa-fw mr-2 text-gray-400"></i>
+                  Alterar Senha
+                </a>                
+
