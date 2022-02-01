e257795a6bdeefacbdbd25d1badcb4aacefa18a7
Thiago Adriano Coleti <thiago.coleti@uenp.edu.br>
2022-02-01 15:02:00 -0300

    Controle de Vertebrados

--- lista_vertebrado.php
+++ lista_vertebrado.php
@@ -1,3 +1,14 @@
+
+<?php
+ session_start();
+
+ if ( $_SESSION["usuario"] == "") {
+   header("Location:/cadetutatu/php/limpasession.php");
+ }
+
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -51,10 +62,26 @@
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <span class="mr-2 d-none d-lg-inline text-gray-600 small">Usuário: Valerie Luna</span>              </a>
+                <?php
+                  $perfil = "";
+                  if ($_SESSION["tipo"] == "A"){
+                    $perfil = "Administrador";
+                  }
+                  else if ($_SESSION["tipo"] == "P"){
+                    $perfil = "PESQUISADOR";
+
+                  }
+
+                  print( $_SESSION["nomeusuario"]." - (".$perfil.")");
+                ?>
+
+                            </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                 <div class="dropdown-divider"></div>
+                 <?php
+                  include "menu_usuario.php";
+                ?>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                   Sair
@@ -116,8 +143,7 @@
     <i class="fas fa-angle-up"></i>
   </a>
 
-  <!-- Logout Modal-->
-  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+ <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
@@ -125,11 +151,10 @@
           <button class="close" type="button" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
           </button>
-        </div>
-        <div class="modal-body">Selecione sair para deslogar.</div>
+        </div>        
         <div class="modal-footer">
-          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-          <a class="btn btn-primary" href="login.html">Logout</a>
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Não</button>
+          <a href="index.php" class="btn btn-primary">Sim</a>
         </div>
       </div>
     </div>
